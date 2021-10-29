eff494839ef217dff9d2f5733d22c30950950276
Andres Sanchez <Bunny_HoPp99@hotmail.com>
2021-10-28 17:46:04 -0700

    dbz

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,25 @@
+import { Component} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+
+export class MainPageComponent {
+
+    nuevo: Personaje = {
+    nombre: 'Rochi',
+    poder: 1000
+  }
+
+  get personajes():Personaje[] {
+    return this.DbzService.personajes;
+  }
+
+
+  constructor( private DbzService: DbzService ) {}
+
+}
